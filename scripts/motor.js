fef631371e7f084319bbe0b3dc1c948753bf1684
TheLordRalph <raulmoretamartin@gmail.com>
2022-12-08 10:26:33 +0100

    Maquetacion pagina 'rooms.html' terminada.

--- scripts/motor.js
+++ scripts/motor.js
@@ -74,3 +74,21 @@ var swiper3 = new Swiper(".swiper--square", {
     dynamicBullets: true,
   },
 });
+
+
+// Swiper Rooms
+
+var swiperRooms = new Swiper(".swiperRooms", {
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
